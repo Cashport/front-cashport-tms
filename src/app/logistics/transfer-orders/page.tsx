fbca9a648b28327b25e194d3a125f90f067fb933
Marcos <84048657+Rodriguez-Marcos@users.noreply.github.com>
2024-10-31 12:32:44 -0300

    hotfix suspense

--- src/app/logistics/transfer-orders/page.tsx
+++ src/app/logistics/transfer-orders/page.tsx
@@ -1,11 +1,15 @@
 "use client";
+import Loader from "@/components/atoms/loaders/loader";
 import { TransferOrders } from "@/components/organisms/logistics/transfer-orders/TransferOrders";
 import ViewWrapper from "@/components/organisms/ViewWrapper/ViewWrapper";
+import { Suspense } from "react";
 
 function TransferOrderPage() {
   return (
     <ViewWrapper headerTitle="Ordenes de transferencia">
-      <TransferOrders />
+      <Suspense fallback={<Loader />}>
+        <TransferOrders />
+      </Suspense>
     </ViewWrapper>
   );
 }
