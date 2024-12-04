de749666930a4dab644e19ca281b5b2e27b6098f
cdramirezp98 <cdramirezp98@gmail.com>
2024-12-04 14:43:31 -0500

    Update download_csv.ts

--- src/services/logistics/download_csv.ts
+++ src/services/logistics/download_csv.ts
@@ -6,18 +6,20 @@ export async function downloadCSVFromEndpoint(endpoint: string, filename: string
   try {
     const projectId = await getProjectId();
     const token = await getIdToken();
+
     const response = await axios.get(`${config.API_HOST}/${endpoint}`, {
-      responseType: "text",
+      responseType: "blob",
       headers: {
-        "Content-Type": "text/csv",
+        "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         Authorization: `Bearer ${token}`,
         projectId: `${projectId}`
       }
     });
-    const csvData = response.data;
 
-    // Crear un Blob con los datos CSV
-    const blob = new Blob([`\uFEFF${csvData}`], { type: "text/csv;charset=utf-8;" });
+    const blob = new Blob([response.data], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    });
+
     const link = document.createElement("a");
     const url = URL.createObjectURL(blob);
     link.href = url;
@@ -26,6 +28,7 @@ export async function downloadCSVFromEndpoint(endpoint: string, filename: string
     link.click();
     document.body.removeChild(link);
   } catch (error) {
-    console.error("Error downloading the CSV file:", error);
+    console.error("Error downloading the XLSX file:", error);
   }
 }
+
