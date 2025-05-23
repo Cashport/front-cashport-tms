f90287f12c6f9b8dda34a6cfe72b7b65713aa493
FedericoC <federicoc03@gmail.com>
2025-05-23 11:24:14 -0500

    feat: add audit requirements and document management functions

---
+++ src/services/logistics/providers/providers.ts
@@ -0,0 +1,65 @@
+import { IAuditTableRow } from "@/components/organisms/logistics/proveedores/ModalAuditRequirements/ModalAuditRequirements";
+import { GenericResponse } from "@/types/global/IGlobal";
+import { API } from "@/utils/api/api";
+
+export const auditRequirements = async (documents: IAuditTableRow[]) => {
+  try {
+    const body = {
+      documents: documents.map((doc) => ({
+        documentTypeSubjectId: doc.id,
+        accept: doc.audit?.toLowerCase() === "aprobar" ? true : false,
+        comments: doc.commentary ?? ""
+      }))
+    };
+
+    const response = await API.post("/subject/audit", body);
+    return response;
+  } catch (error) {
+    console.error("Error al auditar requerimientos:", error);
+    throw error;
+  }
+};
+
+export interface IDocument {
+  id: number;
+  project_id: number;
+  document_type: string;
+  name: string;
+  template_url: string | null;
+}
+
+export const getAvailableDocumentTypes = async (subjectId: number) => {
+  try {
+    const response: GenericResponse<IDocument[]> = await API.get(
+      `/subject/${subjectId}/available-document-types`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener documentos disponibles:", error);
+    throw error;
+  }
+};
+
+export const createDocumentBySubjectId = async (subjectId: any, documentTypeId: any) => {
+  try {
+    const response: GenericResponse<any> = await API.post(
+      `/subject/${subjectId}/documents-type/${documentTypeId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al crear documento por ID de sujeto:", error);
+    throw error;
+  }
+};
+
+export const deleteDocumentById = async (subjectId: string, documentTypeId: number) => {
+  try {
+    const response: GenericResponse<any> = await API.delete(
+      `/subject/${subjectId}/documents-type/${documentTypeId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar documento por ID:", error);
+    throw error;
+  }
+};
