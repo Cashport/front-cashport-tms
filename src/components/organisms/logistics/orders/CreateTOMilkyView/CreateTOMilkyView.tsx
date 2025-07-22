f26abb5ce58b0918ef344f2a15c8be1e490aa809
FedericoC <federicoc03@gmail.com>
2025-07-21 19:25:51 -0500

    Enhance CreateTOMilkyView with driver phone copy functionality, improved layout, and new stop item display

--- src/components/organisms/logistics/orders/CreateTOMilkyView/CreateTOMilkyView.tsx
+++ src/components/organisms/logistics/orders/CreateTOMilkyView/CreateTOMilkyView.tsx
@@ -3,8 +3,16 @@ import Image from "next/image";
 import dayjs from "dayjs";
 import "dayjs/locale/es"; // Importar locale español
 import utc from "dayjs/plugin/utc";
-import { Button, Flex, Tooltip } from "antd";
-import { CaretCircleDoubleUp, Copy, Phone, Star } from "@phosphor-icons/react";
+import { Button, Dropdown, Flex, MenuProps, Tooltip } from "antd";
+import {
+  CaretCircleDoubleUp,
+  CaretDown,
+  Copy,
+  HandPalm,
+  Phone,
+  Star,
+  Truck
+} from "@phosphor-icons/react";
 
 import Container from "@/components/atoms/Container/Container";
 import PrincipalButton from "@/components/atoms/buttons/principalButton/PrincipalButton";
@@ -41,104 +49,144 @@ const CreateTOMilkyView: React.FC = () => {
         </div>
 
         <Flex vertical gap={"1.5rem"}>
-          {mockRoutes.map((route) => (
-            <div key={route.id} className="createTOMilkyView__recommendationTripCard">
-              <Flex
-                className="createTOMilkyView__recommendationTripCard__header"
-                gap={"1rem"}
-                align="center"
-                justify="space-between"
-              >
-                <Flex gap={"0.75rem"} align="center">
-                  <Image
-                    src={route.companyInfo.companyLogo}
-                    alt={route.companyInfo.companyName}
-                    width={50}
-                    height={50}
-                  />
-                  <Flex vertical>
-                    <h6>{route.tripDescription}</h6>
-                    <p>{route.companyInfo.companyName}</p>
-                  </Flex>
-                </Flex>
-
-                <Flex gap={"1.25rem"} align="center">
-                  {route.isRecommended && (
-                    <Flex gap={"0.5rem"} align="center">
-                      <Star size={12} />
-                      <p>Recomendado</p>
-                    </Flex>
-                  )}
-
-                  {route.dedicatedFleet && (
-                    <Flex gap={"0.5rem"} align="center">
-                      <CaretCircleDoubleUp size={12} />
-                      <p>Flota dedicada</p>
-                    </Flex>
-                  )}
-
-                  {route.driversInfo && (
-                    <Tooltip
-                      className="driver-info-tooltip"
-                      title={
-                        <Flex
-                          gap={"1rem"}
-                          align="center"
-                          className="driver-info-tooltip-hoverMessage"
-                        >
-                          <p className="driver-phone">{route.driversInfo.driverPhone}</p>
-                          <Button
-                            onClick={() => handleCopyDriverPhone(route.driversInfo.driverPhone)}
-                            className="copyButton"
-                          >
-                            <Copy size={16} />
-                          </Button>
-                        </Flex>
-                      }
-                      color="#ffffff"
-                    >
-                      <Phone size={12} />
-                      <p className="driver-name">{route.driversInfo.driverName}</p>
-                    </Tooltip>
-                  )}
-                </Flex>
-              </Flex>
-
-              <div className="createTOMilkyView__recommendationTripCard__content">
-                <Flex justify="space-between">
-                  <Flex vertical gap={"0.5rem"}>
-                    <p className="date">{dayjs(route.tripInfo.originTime).format("D MMM YYYY")}</p>
-                    <h5 className="time">{dayjs(route.tripInfo.originTime).format("HH:mm")}</h5>
-                    <p className="origin">
-                      {route.tripInfo.origin} <span>({route.tripInfo.originCity})</span>
-                    </p>
-                  </Flex>
-                  <div>Aca va la ruta</div>
-
-                  <Flex vertical gap={"0.5rem"}>
-                    <p className="date">
-                      {dayjs(route.tripInfo.destinationTime).format("D MMM YYYY")}
-                    </p>
-                    <h5 className="time">
-                      {dayjs(route.tripInfo.destinationTime).format("HH:mm")}
-                    </h5>
-                    <p className="origin">
-                      {route.tripInfo.destination} <span>({route.tripInfo.destinationCity})</span>
-                    </p>
-                  </Flex>
-                </Flex>
-
-                <Flex vertical gap={"0.5rem"} align="center" justify="center">
-                  <PrincipalButton
-                    style={{ width: "151px", height: "45px" }}
-                    className="selectRouteButton"
+          {mockRoutes.map((route) => {
+            const stopsItems: MenuProps["items"] = route?.stops?.map((stop, i) => ({
+              key: stop.id,
+              label: (
+                <>
+                  <div
+                    className="stopItem"
+                    style={{
+                      borderBottom: i === route.stops.length - 1 ? "none" : "1px solid #DDDDDD",
+                      paddingBottom: i === route.stops.length - 1 ? 0 : "0.5rem"
+                    }}
                   >
-                    Seleccionar ruta
-                  </PrincipalButton>
+                    <p>
+                      {i + 1}. {stop.name} <span>({stop.cityName})</span>
+                    </p>
+
+                    <p>{dayjs(stop.time).format("MMM DD - HH:mm")}</p>
+                  </div>
+                </>
+              )
+            }));
+            return (
+              <div key={route.id} className="createTOMilkyView__recommendationTripCard">
+                <Flex
+                  className="createTOMilkyView__recommendationTripCard__header"
+                  gap={"1rem"}
+                  align="center"
+                  justify="space-between"
+                >
+                  <Flex gap={"0.75rem"} align="center">
+                    <Image
+                      src={route.companyInfo.companyLogo}
+                      alt={route.companyInfo.companyName}
+                      width={50}
+                      height={50}
+                    />
+                    <Flex vertical>
+                      <h6>{route.tripDescription}</h6>
+                      <p>{route.companyInfo.companyName}</p>
+                    </Flex>
+                  </Flex>
+
+                  <Flex gap={"1.25rem"} align="center">
+                    {route.isRecommended && (
+                      <Flex gap={"0.5rem"} align="center">
+                        <Star size={12} />
+                        <p>Recomendado</p>
+                      </Flex>
+                    )}
+
+                    {route.dedicatedFleet && (
+                      <Flex gap={"0.5rem"} align="center">
+                        <CaretCircleDoubleUp size={12} />
+                        <p>Flota dedicada</p>
+                      </Flex>
+                    )}
+
+                    {route.driversInfo && (
+                      <Tooltip
+                        className="driver-info-tooltip"
+                        title={
+                          <Flex
+                            gap={"1rem"}
+                            align="center"
+                            className="driver-info-tooltip-hoverMessage"
+                          >
+                            <p className="driver-phone">{route.driversInfo.driverPhone}</p>
+                            <Button
+                              onClick={() => handleCopyDriverPhone(route.driversInfo.driverPhone)}
+                              className="copyButton"
+                            >
+                              <Copy size={16} />
+                            </Button>
+                          </Flex>
+                        }
+                        color="#ffffff"
+                      >
+                        <Phone size={12} />
+                        <p className="driver-name">{route.driversInfo.driverName}</p>
+                      </Tooltip>
+                    )}
+                  </Flex>
                 </Flex>
+
+                <div className="createTOMilkyView__recommendationTripCard__content">
+                  <Flex gap={"1.5rem"} justify="space-between">
+                    <Flex vertical gap={"0.5rem"}>
+                      <p className="date">
+                        {dayjs(route.tripInfo.originTime).format("D MMM YYYY")}
+                      </p>
+                      <h5 className="time">{dayjs(route.tripInfo.originTime).format("HH:mm")}</h5>
+                      <p className="origin">
+                        {route.tripInfo.origin} <span>({route.tripInfo.originCity})</span>
+                      </p>
+                    </Flex>
+                    <Flex vertical gap={"0.5rem"} align="center" style={{ flexGrow: 1 }}>
+                      <span className="hhmmDuration">38h 45m</span>
+                      <Flex style={{ width: "100%" }} align="center">
+                        <Truck size={24} />
+                        <span className="durationLine" />
+                      </Flex>
+
+                      {stopsItems && stopsItems.length > 0 && (
+                        <Dropdown menu={{ items: stopsItems }} placement="bottom">
+                          <Button>
+                            <HandPalm size={16} />
+                            Paradas {stopsItems.length}
+                            <CaretDown size={16} />
+                          </Button>
+                        </Dropdown>
+                      )}
+                    </Flex>
+
+                    <Flex vertical gap={"0.5rem"}>
+                      <p className="date">
+                        {dayjs(route.tripInfo.destinationTime).format("D MMM YYYY")}
+                      </p>
+                      <h5 className="time">
+                        {dayjs(route.tripInfo.destinationTime).format("HH:mm")}
+                      </h5>
+                      <p className="origin">
+                        {route.tripInfo.destination} <span>({route.tripInfo.destinationCity})</span>
+                      </p>
+                    </Flex>
+                  </Flex>
+
+                  <Flex vertical gap={"0.5rem"} align="center" justify="center">
+                    <PrincipalButton
+                      style={{ width: "151px", height: "45px" }}
+                      className="selectRouteButton"
+                    >
+                      Seleccionar ruta
+                    </PrincipalButton>
+                  </Flex>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </Flex>
       </div>
     </Container>
