46ddae029e5966b7acc798b79a926212438da9f4
FedericoC <federicoc03@gmail.com>
2025-07-24 17:13:54 -0500

    Refactor CreateTOMilkyView layout and styling for improved consistency and responsiveness

--- src/components/organisms/logistics/orders/CreateTOMilkyView/CreateTOMilkyView.tsx
+++ src/components/organisms/logistics/orders/CreateTOMilkyView/CreateTOMilkyView.tsx
@@ -32,7 +32,7 @@ const CreateTOMilkyView: React.FC = () => {
       <Container>
         <div className="createTOMilkyView">
           <div className="createTOMilkyView__descriptionCard">
-            <Flex vertical gap={"1rem"} style={{ maxWidth: "575px" }}>
+            <Flex vertical gap={"0.25rem"}>
               <h3>Flota dedicada</h3>
               <h5>Identificamos vehículos con capacidad disponible en la ruta de tu carga.</h5>
               <p>
@@ -44,8 +44,8 @@ const CreateTOMilkyView: React.FC = () => {
             <Image
               src="/images/logistics/createTODescriptionImage.png"
               alt="Create TO Description"
-              width={595}
-              height={250}
+              width={215}
+              height={90}
             />
           </div>
 
