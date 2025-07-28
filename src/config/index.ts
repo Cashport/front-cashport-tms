1931a5cf0a3fbe0f3da00c3e2843aa0432b7a1b2
Carlos Ramirez <cdramirezp98@gmail.com>
2025-07-28 18:57:50 -0500

    Add audit driver

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ const config = {
   API_HOST: process.env.NEXT_PUBLIC_API_HOST,
   isLogistics: Boolean(process.env.NEXT_PUBLIC_IS_LOGISTICS),
   CORE_API_HOST: process.env.NEXT_PUBLIC_CORE_API_HOST,
+  API_AUDIT_TAB_AI: process.env.NEXT_PUBLIC_API_AUDIT_AI,
 };
 
 export default config;
