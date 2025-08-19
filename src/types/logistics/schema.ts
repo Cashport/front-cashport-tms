8f2328534dee203a43057900bed9bc668f0d8c10
FedericoC <federicoc03@gmail.com>
2025-08-19 17:14:56 -0500

    Enhance CarrierRequestProposal with links for vehicle and driver details; update CarriersPricing interface to include driver_id

--- src/types/logistics/schema.ts
+++ src/types/logistics/schema.ts
@@ -2057,6 +2057,7 @@ export interface CarriersPricing {
   driver_delay: number;
   diver_trips: number;
   driver_score: number;
+  driver_id: number;
   id: number;
   id_carrier: number;
   id_service_type: number;
